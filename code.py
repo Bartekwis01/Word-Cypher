8ba607526a92f216945b2b8747a6b4f3eaa1cc6a
Bartekwis01 <Bartekwis01@gmail.com>
2025-08-08 10:47:54 +0200

    Add exit codes and change the names of some variables to follow correct naming scheme

--- code.py
+++ code.py
@@ -4,7 +4,7 @@ import random
 import json
 
 PLAINTEXT_PATH = 'plaintext.txt'
-TEXT_CODED_PATH = 'coded-text.txt'
+CODED_TEXT_PATH = 'coded-text.txt'
 DICTIONARY_PATH = 'dictionary.txt'
 WORDS_PATH = 'words.txt'
 WORDS = []
@@ -15,7 +15,7 @@ if os.path.exists(PLAINTEXT_PATH):
 else:
     print(f'No plaintext file detected. Please create a file named {PLAINTEXT_PATH} and run the program again.')
     input('Press Enter to close...')
-    sys.exit()
+    sys.exit(2)
 if os.path.exists(WORDS_PATH):
     with open(WORDS_PATH, 'r') as file:
         WORDS = []
@@ -25,9 +25,9 @@ if os.path.exists(WORDS_PATH):
 else:
     print(f'No words file detected. Please move a file named {WORDS_PATH} into the same folder as the app and run the program again. This is the file that is recommended: https://github.com/dwyl/english-words/blob/master/words_alpha.txt, but others may work provided that they are in the same format.')
     input('Press Enter to close...')
-    sys.exit()
-if os.path.exists(TEXT_CODED_PATH):
-    print(f'{TEXT_CODED_PATH} file detected. Please make sure that it is okay to overwrite that file. In case you want to preserve that file, please close the program and move that file to another location, otherwise press Enter to continue...')
+    sys.exit(2)
+if os.path.exists(CODED_TEXT_PATH):
+    print(f'{CODED_TEXT_PATH} file detected. Please make sure that it is okay to overwrite that file. In case you want to preserve that file, please close the program and move that file to another location, otherwise press Enter to continue...')
     input('Press Enter to continue, otherwise close the program.')
 if os.path.exists(DICTIONARY_PATH):
     with open(DICTIONARY_PATH, 'r') as file:
@@ -35,8 +35,6 @@ if os.path.exists(DICTIONARY_PATH):
 else:
     print(f'WARNING - no dictionary file detected. The program will create a new one. This is normal when running the program for the first time.')
     input(f'Press Enter to acknowledge the warning and create a {DICTIONARY_PATH} file...')
-    with open(DICTIONARY_PATH, 'w') as file:
-        pass  # file created
     # noinspection PyRedeclaration
     DICTIONARY = {}
 with open(WORDS_PATH, 'r') as file:
@@ -47,7 +45,7 @@ with open(WORDS_PATH, 'r') as file:
 
 
 def code():
-    text_coded = ""
+    coded_text = ""
     for word in PLAINTEXT.split():
         if word in DICTIONARY.keys():  # .keys()
             coded_word = DICTIONARY[word]
@@ -59,15 +57,15 @@ def code():
                 else:
                     break
             coded_word = random_word
-        text_coded += coded_word
-        text_coded += " "
+        coded_text += coded_word
+        coded_text += " "
         DICTIONARY.update({word: coded_word})
-    with open(TEXT_CODED_PATH, 'w') as file:
-        file.write(text_coded)
+    with open(CODED_TEXT_PATH, 'w') as file:
+        file.write(coded_text)
     with open(DICTIONARY_PATH, 'w') as file:
         file.write(json.dumps(DICTIONARY))
-    print(f'Coded text saved in {TEXT_CODED_PATH}')
+    print(f'Coded text saved in {CODED_TEXT_PATH}')
     input('Press Enter to continue...')
-    sys.exit()
+    sys.exit(0)
 
 code()
