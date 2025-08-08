f5fb95f84a8fc5e4cc500c09db6fa90d1a12952f
Bartekwis01 <Bartekwis01@gmail.com>
2025-08-08 22:03:27 +0200

    Improve naming consistency

--- decode.py
+++ decode.py
@@ -3,7 +3,7 @@ import sys
 import json
 
 DECODED_TEXT_PATH = 'decoded-text.txt'
-TEXT_CODED_PATH = 'coded-text.txt'
+CODED_TEXT_PATH = 'coded-text.txt'
 DICTIONARY_PATH = 'dictionary.txt'
 
 
@@ -13,12 +13,12 @@ def manual_run():
             f'{DECODED_TEXT_PATH} file detected. Please make sure that it is okay to overwrite that file. In case you '
             f'want to preserve that file, please close the program and move that file to another location.')
         input('Press Enter to continue, otherwise close the program.')
-    if os.path.exists(TEXT_CODED_PATH):
-        with open(TEXT_CODED_PATH, 'r', encoding='utf-8') as file:
-            TEXT_CODED = file.read()
+    if os.path.exists(CODED_TEXT_PATH):
+        with open(CODED_TEXT_PATH, 'r', encoding='utf-8') as file:
+            CODED_TEXT = file.read()
     else:
         print(
-            f'ERROR - no coded file detected. Please move a file named {TEXT_CODED_PATH} into the same folder as the app '
+            f'ERROR - no coded file detected. Please move a file named {CODED_TEXT_PATH} into the same folder as the app '
             f'and run the program again')
         input('Press Enter to close...')
         sys.exit(2)
@@ -31,34 +31,33 @@ def manual_run():
             f'app and run the program again. You can automatically generate one by coding a file using code.py program.')
         input(f'Press Enter to close...')
         sys.exit(2)
-    decode(TEXT_CODED, DICTIONARY, DO_SAVE=True)
+    decode(CODED_TEXT, DICTIONARY, DO_SAVE=True)
 
 
-def decode(TEXT_CODED, DICTIONARY, DO_PRINT=True, DO_SAVE=False):
+def decode(CODED_TEXT, DICTIONARY, DO_PRINT=True, DO_SAVE=False):
     word_not_found = False
     decoded_words = []
     DICTIONARY_KEYS = list(DICTIONARY.keys())
     DICTIONARY_VALUES = list(DICTIONARY.values())
-    for word in TEXT_CODED.split():
+    for word in CODED_TEXT.split():
         if word in DICTIONARY_VALUES:
-            word_decoded = DICTIONARY_KEYS[DICTIONARY_VALUES.index(word)]
+            decoded_word = DICTIONARY_KEYS[DICTIONARY_VALUES.index(word)]
         else:
-            word_decoded = 'NOT FOUND' # The definition of the coded word was not found in the dictionary
+            decoded_word = 'NOT FOUND'  # The definition of the coded word was not found in the dictionary
             word_not_found = True
-        decoded_words.append(word_decoded)
-        text_decoded = ' '.join(decoded_words)
-
+        decoded_words.append(decoded_word)
+    decoded_text = ' '.join(decoded_words)
     if word_not_found and DO_PRINT:
         print(f'WARNING - at least one definition of a coded word was not found, it has been replaced with "|NOT '
               f'FOUND|" in the decoded text.')
     if DO_SAVE:
         with open(DECODED_TEXT_PATH, 'w', encoding='utf-8') as file:
-            file.write(text_decoded)
+            file.write(decoded_text)
     if DO_PRINT and DO_SAVE:
         print(f'Decoded text saved in {DECODED_TEXT_PATH}')
         input(f'Press Enter to exit...')
         sys.exit(0)
-    return text_decoded
+    return decoded_text
 
 
 if __name__ == '__main__':
