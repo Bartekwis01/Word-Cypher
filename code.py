993d067ef7dec8248b57a1270532ba1a5a69be54
Bartekwis01 <Bartekwis01@gmail.com>
2025-08-08 20:26:17 +0200

    Restructure code to make it work as a package, slight reformatting

--- code.py
+++ code.py
@@ -9,49 +9,57 @@ DICTIONARY_PATH = 'dictionary.txt'
 WORDS_PATH = 'words.txt'
 WORDS = []
 
-if os.path.exists(PLAINTEXT_PATH):
-    with open(PLAINTEXT_PATH, 'r', encoding='utf-8') as file:
-        PLAINTEXT = file.read()
-else:
-    print(f'No plaintext file detected. Please create a file named {PLAINTEXT_PATH} and run the program again.')
-    input('Press Enter to close...')
-    sys.exit(2)
-if os.path.exists(WORDS_PATH):
-    with open(WORDS_PATH, 'r', encoding='utf-8') as file:
-        WORDS = []
-        for line in file:
-            word = line.strip()
-            WORDS.append(word)
-else:
-    print(f'No words file detected. Please move a file named {WORDS_PATH} into the same folder as the app and run the '
-          f'program again. This is the file that is recommended: '
-          f'https://github.com/dwyl/english-words/blob/master/words_alpha.txt, but others may work provided that they '
-          f'are in the same format.')
-    input('Press Enter to close...')
-    sys.exit(2)
-if os.path.exists(CODED_TEXT_PATH):
-    print(f'{CODED_TEXT_PATH} file detected. Please make sure that it is okay to overwrite that file. In case you '
-          f'want to preserve that file, please close the program and move that file to another location, '
-          f'otherwise press Enter to continue...')
-    input('Press Enter to continue, otherwise close the program.')
-if os.path.exists(DICTIONARY_PATH):
-    with open(DICTIONARY_PATH, 'r', encoding='utf-8') as file:
-        DICTIONARY = json.load(file)
-else:
-    print(f'WARNING - no dictionary file detected. The program will create a new one. This is normal when running the '
-          f'program for the first time.')
-    input(f'Press Enter to acknowledge the warning and create a {DICTIONARY_PATH} file...')
-    # noinspection PyRedeclaration
-    DICTIONARY = {}
-with open(WORDS_PATH, 'r', encoding='utf-8') as file:
-    WORDS = []
-    for line in file:
-        word = line.strip()
-        WORDS.append(word)
+
+def manual_run(DO_PRINT, DO_SAVE):
+    if os.path.exists(PLAINTEXT_PATH):
+        with open(PLAINTEXT_PATH, 'r', encoding='utf-8') as file:
+            PLAINTEXT = file.read()
+    else:
+        print(f'No plaintext file detected. Please create a file named {PLAINTEXT_PATH} and run the program again.')
+        input('Press Enter to close...')
+        sys.exit(2)
+    if os.path.exists(CODED_TEXT_PATH):
+        print(f'{CODED_TEXT_PATH} file detected. Please make sure that it is okay to overwrite that file. In case you '
+              f'want to preserve that file, please close the program and move that file to another location, '
+              f'otherwise press Enter to continue...')
+        input('Press Enter to continue, otherwise close the program.')
+    if os.path.exists(DICTIONARY_PATH):
+        with open(DICTIONARY_PATH, 'r', encoding='utf-8') as file:
+            DICTIONARY = json.load(file)
+    else:
+        print(
+            f'WARNING - no dictionary file detected. The program will create a new one. This is normal when running the '
+            f'program for the first time.')
+        input(f'Press Enter to acknowledge the warning and create a {DICTIONARY_PATH} file...')
+        # noinspection PyRedeclaration
+        DICTIONARY = {}
+    code(PLAINTEXT, DICTIONARY, DO_PRINT=DO_PRINT, DO_SAVE=DO_SAVE)
 
 
-def code():
-    coded_text = ""
+def read_words(DO_PRINT):
+    if os.path.exists(WORDS_PATH):
+        with open(WORDS_PATH, 'r', encoding='utf-8') as file:
+            for line in file:
+                word = line.strip()
+                WORDS.append(word)
+        return WORDS
+    else:
+        if DO_PRINT:
+            print(
+                f'No words file detected. Please move a file named {WORDS_PATH} into the same folder as the app and run the '
+                f'program again. This is the file that is recommended: '
+                f'https://github.com/dwyl/english-words/blob/master/words_alpha.txt, but others may work provided that they '
+                f'are in the same format.')
+            input('Press Enter to close...')
+        sys.exit(2)
+
+
+def code(PLAINTEXT, DICTIONARY=None, WORDS=None, DO_PRINT=False, DO_SAVE=False):
+    if DICTIONARY is None:
+        DICTIONARY = {}
+    if WORDS is None:
+        WORDS = read_words(DO_PRINT)
+    coded_text = ''
     for word in PLAINTEXT.split():
         if word in DICTIONARY.keys():  # .keys()
             coded_word = DICTIONARY[word]
@@ -66,12 +74,17 @@ def code():
         coded_text += coded_word
         coded_text += " "
         DICTIONARY.update({word: coded_word})
-    with open(CODED_TEXT_PATH, 'w', encoding='utf-8') as file:
-        file.write(coded_text)
-    with open(DICTIONARY_PATH, 'w', encoding='utf-8') as file:
-        file.write(json.dumps(DICTIONARY))
-    print(f'Coded text saved in {CODED_TEXT_PATH}')
-    input('Press Enter to continue...')
-    sys.exit(0)
+    if DO_SAVE:
+        with open(CODED_TEXT_PATH, 'w', encoding='utf-8') as file:
+            file.write(coded_text)
+        with open(DICTIONARY_PATH, 'w', encoding='utf-8') as file:
+            file.write(json.dumps(DICTIONARY))
+    if DO_PRINT and DO_SAVE:
+        print(f'Coded text saved in {CODED_TEXT_PATH}')
+        input('Press Enter to continue...')
+        sys.exit(0)
+    return coded_text, DICTIONARY
 
-code()
+
+if __name__ == '__main__':
+    manual_run(DO_PRINT=True, DO_SAVE=True)
